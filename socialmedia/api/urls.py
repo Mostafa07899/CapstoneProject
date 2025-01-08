a8e418f57444a7c6aee375cac051e83d569f501a
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2025-01-08 19:30:56 +0200

    all done

--- socialmedia/api/urls.py
+++ socialmedia/api/urls.py
@@ -5,7 +5,7 @@ from .views import UserDetailView, UserListCreateView, PostDetailView, PostlistC
 urlpatterns = [
     path('users/', UserListCreateView.as_view(), name='user_list_create'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
-    path('pots/', PostlistCreateView.as_view(), name='post_list_create'),
+    path('posts/', PostlistCreateView.as_view(), name='post_list_create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('follow/<int:pk>/', FollowView.as_view(), name='follow'),
     path('feed/', FeedView.as_view(), name='feed'),
