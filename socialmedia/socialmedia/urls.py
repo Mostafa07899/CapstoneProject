9327e752d09c537ef3758d350825db7e0228aeec
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2025-01-08 20:04:05 +0200

    removed a line

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api import views
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenObtainPairView,
