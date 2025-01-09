5f59cc30fc90135142835d27a2c6e2d2534a5ce7
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2025-01-09 18:05:01 +0200

    all works

--- socialmedia/api/urls.py
+++ socialmedia/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import UserDetailView, UserListCreateView, PostDetailView, PostlistCreateView, FollowView, FeedView
 
 
